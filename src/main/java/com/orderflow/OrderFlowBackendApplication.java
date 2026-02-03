cbfc1d4857448cbddb7bac0aa63ae83591346690
daksh8492 <dakshtomar386@gmail.com>
2026-02-03 13:02:47 +0530

    First Commit

---
+++ src/main/java/com/orderflow/OrderFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.orderflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFlowBackendApplication.class, args);
+	}
+
+}
